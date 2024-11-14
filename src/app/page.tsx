40d4c2cc1ad6c3b173f671a13f0b032119faaa40
MrBeeni <mubeen4429@gmail.com>
2024-11-14 10:22:19 +0500

    setting up theme and assets

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ipsum quae
+      consectetur, vel ratione natus nostrum ex accusamus quasi, ut, ea veniam
+      quia laudantium sint nemo tempore numquam soluta laboriosam!
+    </div>
+  );
+}
