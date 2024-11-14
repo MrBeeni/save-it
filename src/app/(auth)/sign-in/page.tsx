a8a814da2f75a0aee79d0b8b328c4ee03256544f
MrBeeni <mubeen4429@gmail.com>
2024-11-14 10:22:43 +0500

    add auth layout and routs

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+// import AuthForm from "@/components/AuthForm";
+
+const SignIn = () =>
+  //  <AuthForm type="sign-in" />;
+  {
+    return <section>page</section>;
+  };
+
+export default SignIn;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+// import AuthForm from "@/components/AuthForm";
+
+const SignUp = () => {
+  return <section>page</section>;
+};
+
+export default SignUp;
