3c3d3821bdd9a0c577962a19fa98048bde4aac7d
MrBeeni <mubeen4429@gmail.com>
2024-11-17 12:44:46 +0500

    setup appwrite sdk | implemented user create action

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { Input } from '@/components/ui/input';
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { createAccount } from '@/actions/user.actions';
 // import { createAccount, signInUser } from "@/lib/actions/user.actions";
 // import OtpModal from "@/components/OTPModal";
 
@@ -55,15 +56,15 @@ const AuthForm = ({ type }: { type: FormType }) => {
 
     try {
       const user =
-        // type === "sign-up"
-        //   ? await createAccount({
-        //       fullName: values.fullName || "",
-        //       email: values.email,
-        //     })
-        //   : await signInUser({ email: values.email });
+        type === 'sign-up'
+          ? await createAccount({
+              fullName: values.fullName || '',
+              email: values.email,
+            })
+          : // : await signInUser({ email: values.email });
 
-        // setAccountId(user.accountId);
-        console.log('first');
+            console.log('first');
+      setAccountId(user.accountId);
     } catch {
       setErrorMessage('Failed to create account. Please try again.');
     } finally {
